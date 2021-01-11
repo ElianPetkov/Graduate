4bbf35f802c3d05492140d66d4ead8095cff7b2f
EPetkov <elianpetkov1@gmail.com>
2021-01-11 15:29:28 +0200

    add favicon

--- views/studentPage.php
+++ views/studentPage.php
@@ -6,6 +6,7 @@
 <head>
     <link rel="stylesheet" href="../css/student.css">
     <link rel="stylesheet" href="../css/main.css">
+    <link href="../pictures/favicon.ico" rel="shortcut icon" type="image/icon" />
 </head>
 
 <body>
