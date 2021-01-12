2a399d2837814e4991f4da085ef6fcd427470e1d
EPetkov <elianpetkov1@gmail.com>
2021-01-12 16:30:39 +0200

    add validation for student ivitation for ceremony

--- logic/studentPage.php
+++ logic/studentPage.php
@@ -1,28 +1,50 @@
 <?php
-    require_once '../config/includeClasses.php';
+require_once '../config/includeClasses.php';
 
+function isCeremonyOver($ceremonyDate)
+{
 
-    try
-    {   
-    $fn = $_GET["fn"];
-    $password = $_GET["password"];
+    $partsOfTodayDate = explode('-', date("Y-m-d"));
+    $partsOfCeremonyDate = explode('-', $ceremonyDate);
+
+    $equalsYears = intval($partsOfTodayDate[0]) == intval($partsOfCeremonyDate[0]);
+    $equalsMonths = intval($partsOfTodayDate[1]) == intval($partsOfCeremonyDate[1]);
+
+    if (intval($partsOfTodayDate[0]) > intval($partsOfCeremonyDate[0])) {
+        return 1;
+    } else if ($equalsYears && intval($partsOfTodayDate[1]) > intval($partsOfCeremonyDate[1])) {
+        return 1;
+    } else if ($equalsYears && $equalsMonths && intval($partsOfTodayDate[2]) > intval($partsOfCeremonyDate[2])) {
+        return 1;
+    }
+
+    return 0;
+}
+
+$fn = $_GET["fn"];
+$password = $_GET["password"];
+
+try {
     $student = new Student($fn, $password);
-    $studentName = $student->getName();
-    //require_once("../views/studentPage.php");
-    }
-    catch(Exception $e)
-    {
-        echo $e->getMessage();
-    }
+} catch (Exception $e) {
+    echo $e->getMessage();
+}
 
-    $ceremony = new Ceremony($student->getClass(),$student->getDegree());
-    var_dump($ceremony);
-    if(isset($ceremony))
-    {
-
-
-    }
+$studentName = $student->getName();
 
 
 
-?>
+try {
+    $ceremony = new Ceremony($student->getClass(), $student->getDegree());
+} catch (Exception $e) {
+    echo $e->getMessage();
+}
+
+$ceremonyDate = $ceremony->getDate();
+if (isCeremonyOver($ceremonyDate)) {
+    $errorMessage = "Церемонията е приключила на " . explode(" ", $ceremonyDate)[0] . " !";
+    $isCeremonyOver = true;
+} else {
+    $isCeremonyOver = false;
+    $address = $ceremony->getAddress();
+}
