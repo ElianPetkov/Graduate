36d01a80de344c02a0285024a4532b86f1f03621
Superheroestuff <37773448+Superheroestuff@users.noreply.github.com>
2021-01-12 14:17:19 +0200

    added class Tasks

---
+++ classes/tasks.php
@@ -0,0 +1,64 @@
+<?php
+require 'ConnectionDB.php';
+
+class Tasks
+{
+    private $fn;
+    private $diploma;
+    private $sign;
+    private $hat;
+    private $grown;
+    private $db;
+
+    public function getTasks($fn)
+    {
+        $select = "Select * From Tasks Where fn=?";
+        try {
+            $conn = $this->db->getConnection();
+            $statement = $conn->prepare($select);
+            $statement->execute([$fn]);
+            return $statement->fetch();
+        } catch (PDOException $error) {
+            echo $error->getMessage();
+            return;
+        }
+    }
+
+    public function __construct($fn)
+    {
+        $this->db = new Db();
+        $tasksData = $this->getTasks($fn);
+        if(is_array($tasksData))
+        {    
+        $this->fn = $tasksData['FN'];
+        $this->diploma = $tasksData['Diploma'];
+        $this->sign = $tasksData['Sign'];
+        $this->hat = $tasksData['Hat'];
+        $this->grown = $tasksData['Grown'];
+        }
+        else
+        {
+            throw new Exception("Student with tasks doesn't exists!");
+        }
+    }
+
+    public function getFN() {
+        return $this->fn;
+    }
+
+    public function getDiploma() {
+        return $this->diploma;
+    }  
+
+    public function getSign() {
+        return $this->sign;
+    }
+
+    public function getHat() {
+        return $this->hat;
+    }
+
+    public function getGrown() {
+        return $this->grown;
+    }
+}
