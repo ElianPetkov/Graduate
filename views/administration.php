120b8e77253b1add372d47855e08655bea17892c
EPetkov <elianpetkov1@gmail.com>
2021-01-13 16:36:06 +0200

    form for ceremony

--- views/administration.php
+++ views/administration.php
@@ -1,5 +1,7 @@
+<?php require '../logic/administration.php' ?>
 <!DOCTYPE html>
 <html>
+
 <head>
     <link rel="stylesheet" href="../css/administration.css">
     <link rel="stylesheet" href="../css/main.css">
@@ -7,7 +9,34 @@
 </head>
 
 <body>
+    <div id="main-container">
+        <div id="student-ceremony-enroll-container">
+            <section id="enroll-ceremony">
+                <form method="POST" action="../logic/administration.php">
+                    <label for="class">Class</label>
+                    <input name="class" type="text" />
 
+
+                    <label for="address">Address</label>
+                    <input name="address" type="text" />
+
+                    <label for="degree">Degree: </label>
+                    <select name="degree">
+                        <option value="Bachelor">Bachelor</option>
+                        <option value="Master">Master</option>
+                        <option value="Doctorate">Doctorate</option>
+                    </select>
+
+                    <label for="dateTime">Дата и час на събитието:</label>
+                    <input type="datetime-local" name="dateTime" value="2018-06-12T19:30">
+
+                    <input type="submit" value="Запиши" />
+                    <input hidden value="ceremony" name="ceremony">
+                </form>
+
+            </section>
+        </div>
+    </div>
 </body>
 
 </html>
