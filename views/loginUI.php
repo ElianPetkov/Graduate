5871abb5f071e59148b43b6f1d30b8c00c218d79
EPetkov <elianpetkov1@gmail.com>
2021-01-15 13:15:48 +0200

    add css to login page

--- views/loginUI.php
+++ views/loginUI.php
@@ -2,18 +2,21 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <link rel="stylesheet" href="../css/student.css">
+    <!--<link rel="stylesheet" href="../css/student.css">
     <link rel="stylesheet" href="../css/diplomаs.css">
-    <link rel="stylesheet" href="../css/main.css">
+    <link rel="stylesheet" href="../css/main.css"> -->
+    <link rel="stylesheet" href="../css/login.css">
     <link href="../pictures/favicon.ico" rel="shortcut icon" type="image/icon" />
 </head>
 
 <body>
+    <div id="background">
     <div id="main-container">
         <section id="greetings-container">
             <h1>
-                Влезте в своя профил:
+                Влезте в своя профил
             </h1>
+            <hr>
             <form  action = "loginUI.php" method = "POST">
             <input type = "text" name = "user" placeholder = "Потребителско име" required autofocus></br>
             <input type = "password"  name = "password" placeholder = "Парола" required></br>
@@ -22,6 +25,7 @@
         <?php
             if($isValid == false) echo '<p>Невалидно потребителско име или парола!</p>';
         ?>
+    </div>
     </div>
 </body>
 
